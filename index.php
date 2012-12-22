470fa3194b3ea6f5eb03f6d846d91c610549348b
Andrej Ostrowski <post@shiftctrl.de>
2012-12-22 21:27:03 +0100

    v0.2

--- index.php
+++ index.php
@@ -53,10 +53,10 @@
 	 </header>
 
 	 <section>
-  	 <article class="txt">
-    	 <h1>Lorem ipsum dolor ist amet</h1>
-    	 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-  	 </article>
+  	<?php 
+  	 $html = file_get_contents('./app/sites/home.php');
+  	 echo $html;
+  	 ?>
 	 </section>
 	 
 	 <footer>
