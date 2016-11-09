1aa54f3ac28d2f71db83ebf79f3024c087999b9d
Andrej Ostrowski <post@shiftctrl.de>
2016-11-09 11:16:57 +0100

    dist

--- core/ctrl.php
+++ core/ctrl.php
@@ -46,7 +46,7 @@
 	$Url = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 	$UrlApp = explode("/", $Url);
   // configure this array key to your webserver folder structure
-  $App = $UrlApp[3];
+  $App = $UrlApp[2];
 
 
   /**
